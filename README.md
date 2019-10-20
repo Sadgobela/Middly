08c20fe9ecab16ef45d3731315a1c60b735c67d7
Prabu Subramani <prabu.subramani@iga.gov.bh>
2019-10-20 12:11:55 +0300

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Middly-Buyer-UI
-User Interface -Middly
+# Middly Frontnet buyers UI
+
+This project contains the code for Middly Buyer/Store frontend developed in Vennila ReactJs + Apollo GraphQL + AWS Amplify
+
+## Project contains
+Required packages are already added to this project,
+```bash
+1. Default libraries of create-react-app
+2. Apollo Client for Graphql
+3. AWS Amplify
+```
+
+## Installation
+
+Use the package manager [yarn](https://yarnpkg.com/lang/en/) to install packages.
+
+```bash
+yarn or npm install
+```
+## Start Development
+
+To start the project.
+
+```bash
+npm start or yarn start
+```
+
---
+++ src/App.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { withClientState } from 'apollo-link-state';
+import { ApolloLink, Observable } from 'apollo-link';
+import { Auth } from 'aws-amplify';
+
+const cache = new InMemoryCache({
+  cacheRedirects: {
+    Query: {
+      movie: (_, { id }, { getCacheKey }) =>
+        getCacheKey({ __typename: 'Movie', id })
+    }
+  }
+});
+
+const request = async (operation) => {
+  const jwtToken = await Auth.currentSession();
+  operation.setContext({
+    headers: {
+      authorization: jwtToken.idToken.jwtToken
+    }
+  });
+};
+
+const requestLink = new ApolloLink((operation, forward) =>
+  new Observable(observer => {
+    let handle;
+    Promise.resolve(operation)
+      .then(oper => request(oper))
+      .then(() => {
+        handle = forward(operation).subscribe({
+          next: observer.next.bind(observer),
+          error: observer.error.bind(observer),
+          complete: observer.complete.bind(observer),
+        });
+      })
+      .catch(observer.error.bind(observer));
+
+    return () => {
+      if (handle) handle.unsubscribe();
+    };
+  })
+);
+
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors) {
+        console.error(graphQLErrors);
+        // sendToLoggingService(graphQLErrors);
+      }
+      if (networkError) {
+        console.error(networkError);
+        // logoutUser();
+      }
+    }),
+    requestLink,
+    withClientState({
+      defaults: {
+        isConnected: true
+      },
+      resolvers: {
+        Mutation: {
+          updateNetworkStatus: (_, { isConnected }, { cache }) => {
+            cache.writeData({ data: { isConnected }});
+            return null;
+          }
+        }
+      },
+      cache
+    }),
+    new HttpLink({
+      uri: 'https://4nef3fumj7.execute-api.eu-west-1.amazonaws.com/dev/graphql',
+      credentials: 'include'
+    })
+  ]),
+  cache
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
