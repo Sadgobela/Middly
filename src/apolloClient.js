f997af6e2e32be8ea71b0abbd93f6fd83c11df35
Prabu Subramani <prabu.subramani@iga.gov.bh>
2019-10-26 11:23:29 +0300

    multiclient added

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -5,6 +5,7 @@ import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { Auth } from 'aws-amplify';
 import { ApolloLink, Observable } from 'apollo-link';
+import { RetryLink } from 'apollo-link-retry';
 
 const cache = new InMemoryCache({
   cacheRedirects: {
@@ -45,6 +46,21 @@ const requestLink = new ApolloLink(
     })
 );
 
+const middlyLink = new HttpLink({
+  uri: 'https://4h5hs4wxt9.execute-api.eu-west-1.amazonaws.com/dev/graphql',
+  credentials: 'include',
+});
+const magentoLink = new HttpLink({
+  uri:
+    ' https://4h5hs4wxt9.execute-api.eu-west-1.amazonaws.com/dev/magento/graphql',
+  credentials: 'include',
+});
+
+const httpLink = new RetryLink().split(
+  (operation) => operation.getContext().clientType === 'magento',
+  magentoLink,
+  middlyLink
+);
 export const client = new ApolloClient({
   link: ApolloLink.from([
     onError(({ graphQLErrors, networkError }) => {
@@ -72,10 +88,11 @@ export const client = new ApolloClient({
       },
       cache,
     }),
-    new HttpLink({
-      uri: 'https://4h5hs4wxt9.execute-api.eu-west-1.amazonaws.com/dev/graphql',
-      credentials: 'include',
-    }),
+    httpLink,
+    // new HttpLink({
+    //   uri: 'https://4h5hs4wxt9.execute-api.eu-west-1.amazonaws.com/dev/graphql',
+    //   credentials: 'include',
+    // }),
   ]),
   cache,
 });
