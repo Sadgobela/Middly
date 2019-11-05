05693de5b6d7a638962f0b31170a6a02f7dc69c8
Prabu Subramani <prabu.subramani@iga.gov.bh>
2019-11-05 12:44:10 +0300

    develop clean up done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import './App.css';
 import { ApolloProvider } from '@apollo/react-hooks';
-import Routes from './Routes';
-
 import apolloClient from './apolloClient';
 
 function App() {
   return (
     <ApolloProvider client={apolloClient}>
-      <div className="App">
-        <Routes />
-      </div>
+      <div className="App">{'Home page'}</div>
     </ApolloProvider>
   );
 }
--- src/components/static/Brands.js
+++
@@ -1,8 +0,0 @@
-export const MIDDDLY_LOGO = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    y="0px"
-    viewBox="0 0 705.6 165"
-    enableBackground="new 0 0 705.6 165"
-  ></svg>
-);
