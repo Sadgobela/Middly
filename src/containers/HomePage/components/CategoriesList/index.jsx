306c993e03a5782a5f85b1ecae12c5ad207d30a7
Sergii Kandyba <seregakandyba@gmail.com>
2019-12-01 11:42:23 +0200

    add side menu, add popups, refactoring of header components

--- src/containers/HomePage/components/CategoriesList/index.jsx
+++ src/containers/HomePage/components/CategoriesList/index.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { arrayOf, string } from 'prop-types';
 import { CategoryItem, CategoriesWrapper } from './styled';
 import { ContentWrapper } from '../../../../globalStyles';
 
-const CategoriesList = ({ list }) => (
-  <ContentWrapper>
-    <CategoriesWrapper justifyContent="flex-start">
-      {list.map(category => (
-        <CategoryItem key={category}>{category}</CategoryItem>
-      ))}
-    </CategoriesWrapper>
-  </ContentWrapper>
-);
+export default class CategoriesList extends Component {
+  state = {
+    current: 'mail',
+  };
+
+  render() {
+    const { list } = this.props;
+
+    return (
+      <ContentWrapper>
+        <CategoriesWrapper justifyContent="flex-start">
+          {list.map(category => (
+            <CategoryItem className="menu-item"
+                          key={category}
+            >
+              {category}
+            </CategoryItem>
+          ))}
+        </CategoriesWrapper>
+      </ContentWrapper>
+    );
+  }
+}
 
 CategoriesList.propTypes = {
   list: arrayOf(string).isRequired
 };
-
-export default CategoriesList;
--- src/containers/HomePage/components/SearchInput/index.jsx
+++ src/containers/HomePage/components/SearchInput/index.jsx
@@ -1,21 +1,117 @@
 import React from 'react';
-import { Select } from 'antd';
+
+import { AutoComplete } from 'antd';
+
+const { Option, OptGroup } = AutoComplete;
 
 import SearchIcon from 'assets/SearchIcon';
 
-import { SearchButton, StyledSelect, StyledInput, SearchContainer } from './styled';
+import { SearchButton, StyledSelect, SearchContainer } from './styled';
 
-const { Option } = Select;
+const dataSource = [
+  {
+    title: 'Libraries',
+    children: [
+      {
+        title: 'AntDesign',
+        count: 10000,
+      },
+      {
+        title: 'AntDesign UI',
+        count: 10600,
+      },
+    ],
+  },
+  {
+    title: 'Solutions',
+    children: [
+      {
+        title: 'AntDesign UI',
+        count: 60100,
+      },
+      {
+        title: 'AntDesign',
+        count: 30010,
+      },
+    ],
+  },
+  {
+    title: 'Articles',
+    children: [
+      {
+        title: 'AntDesign design language',
+        count: 100000,
+      },
+    ],
+  },
+];
+
+function renderTitle(title) {
+  return (
+    <span>
+      {title}
+      <a
+        style={{ float: 'right' }}
+        href="https://www.google.com/search?q=antd"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        more
+      </a>
+    </span>
+  );
+}
+
+const options = dataSource
+  .map(group => (
+    <OptGroup key={group.title} label={renderTitle(group.title)}>
+      {group.children.map(opt => (
+        <Option key={opt.title} value={opt.title}>
+          {opt.title}
+          <span className="certain-search-item-count">{opt.count} people</span>
+        </Option>
+      ))}
+    </OptGroup>
+  ))
+  .concat([
+    <Option disabled key="all" className="show-all">
+      <a href="https://www.google.com/search?q=antd" target="_blank" rel="noopener noreferrer">
+        View all results
+      </a>
+    </Option>,
+  ]);
+
+
+function Complete() {
+  return (
+    <div className="certain-category-search-wrapper">
+      <AutoComplete
+        className="certain-category-search"
+        dropdownClassName="certain-category-search-dropdown"
+        dropdownMatchSelectWidth={false}
+        dropdownStyle={{ width: 300 }}
+        size="large"
+        style={{ width: '100%' }}
+        dataSource={options}
+        placeholder="Thirts, Shoes"
+        optionLabelProp="value"
+      >
+      </AutoComplete>
+
+      <StyledSelect value="Brands">
+        <Option value="Brands">Brands</Option>
+        <Option value="Brands">Brands</Option>
+      </StyledSelect>
+      <SearchButton>
+        <SearchIcon />
+      </SearchButton>
+    </div>
+  );
+}
 
 const SearchInput = () => (
   <SearchContainer>
-    <StyledInput />
-    <StyledSelect value="Brands">
-      <Option value="Brands">Brands</Option>
-    </StyledSelect>
-    <SearchButton>
-      <SearchIcon />
-    </SearchButton>
+    <Complete />
   </SearchContainer>
 );
 
